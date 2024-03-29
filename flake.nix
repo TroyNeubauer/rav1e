39611bb7396098d6c1e847c3aadcfed3e61eac42
Troy Neubauer <troyneubauer@gmail.com>
2024-03-29 11:41:30 -0400

    nixify

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+        oskd = import ./package.nix { pkgs = pkgs; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+            nasm
+          ];
+
+          shellHook = ''
+            export RUST_SRC_PATH="${rust}";
+          '';
+        };
+      }
+    );
+}
